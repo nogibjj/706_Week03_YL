3aecb5a40989b8939b1695b05d275d50ae86b9b3
Yue Li <michaelli0914@outlook.com>
2023-09-18 20:47:03 +0000

    change descriptive.py

--- descriptive.py
+++ descriptive.py
@@ -5,9 +5,10 @@ nba = pl.read_csv("nba-teams-2017.csv")
 nba = nba.drop(columns=["team"])
 print(nba.describe())
 
+
 def gen_stats():
     data = pl.read_csv("nba-teams-2017.csv")
-    data = data.drop["team"]
+    data = data.drop(columns=["team"])
     return data.describe()
 
 
@@ -26,24 +27,6 @@ def build_scatterplot():
     plt.ylabel("Points")
     plt.show()
     return
-
-    # calculate the median/mean/standard deviation for the log wine prices
-    # price_median = df["Price"].dropna().median()
-    # price_mean = df["Price"].dropna().mean()
-    # price_sd = df["Price"].dropna().std()
-    # print("The median log wine price is " + str(price_median))
-    # print("The mean log wine price is " + str(price_mean))
-    # print("The standard deviation for log wine price is " + str(price_sd))
-
-    # visualization: histogram for log wine price
-    # plt.hist(df["Price"], bins=5, edgecolor="k")
-    # plt.xlabel("Log wine price")
-    # plt.ylabel("Frequency")
-    # plt.title("Frequency distribution of log wine price")
-    # plt.show()
-
-    # visualization: correlation plot between columns
-    # sns.pairplot(df)
 
 
 if __name__ == "__main__":
